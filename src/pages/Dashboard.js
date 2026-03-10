3ca7a025e1603bfbd563b67b7a22e015841045c9
meetali2409 <rajputmeetali249@gmail.com>
2026-03-10 09:17:31 +0530

    fix usenavigate import

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 const API = "https://mealmanagement-backend-production.up.railway.app";
 
 function Dashboard() {
